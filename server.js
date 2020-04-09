5730f73c1c3388b863c634009b16f4969c01bf7d
vladyslavnUA <vladyslavn@yahoo.com>
2020-04-09 14:10:09 -0700

    updated files

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-// require libraries
+// require libraries and middleware
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
-
-// middleware
 const exphbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const dotenv = require('dotenv');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// controllers
+// const homeController = require('./controllers/home')(app);
+const postsController = require('./controllers/posts.js')(app);
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// views
+app.get('/', (req, res) => res.render('home'));
+app.get('/posts/new', (req, res) => res.render('posts-new'));
 
 
 app.listen(3000, () => {
