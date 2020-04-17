5c1f63d936125962976cc6da9942298d4107000a
vladyslavnUA <vladyslavn@yahoo.com>
2020-04-16 22:53:46 -0700

    post tests implement

--- models/post.js
+++ models/post.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-  createdAt: { type: Date },
-  updatedAt: { type: Date },
+  // createdAt: { type: Date },
+  // updatedAt: { type: Date },
   title: { type: String, required: true },
   url: { type: String, required: true },
   summary: { type: String, required: true }
