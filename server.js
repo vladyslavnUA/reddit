46bd9323716baf95365db1e4c43bfeaa0f3470b1
vladyslavnUA <vladyslavn@yahoo.com>
2020-04-28 14:56:43 -0700

    users can sign up

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const app = express();
 const exphbs = require('express-handlebars');
 const expressValidator = require('express-validator');
 const dotenv = require('dotenv');
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 dotenv.config({ path: '.env' });
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// var app = express();
 
 // Set db
 require('./data/reddit-db');
@@ -17,17 +21,17 @@ require('./data/reddit-db');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// controllers
-// const homeController = require('./controllers/home')(app);
-require('./controllers/posts')(app);
-require('./controllers/comments.js')(app);
-
 // Add after body parser initialization!
 app.use(expressValidator());
+app.use(cookieParser());
 
 // views
 app.listen(3000, () => {
     console.log('Reddit JS listening on port localhost:3000!');
 });
+
+require('./controllers/posts')(app);
+require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
 
 module.exports = app;
