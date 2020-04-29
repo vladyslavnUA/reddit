e95c885ef35edd5f7be8c2e8f3f1900b20b8c564
vladyslavnUA <vladyslavn@yahoo.com>
2020-04-28 21:15:40 -0700

    Fixed posts test

--- test/posts.js
+++ test/posts.js
@@ -2,6 +2,7 @@ const app = require("./../server");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
+const agent = chai.request.agent(app);
 
 // Import the Post model from our models folder so we
 // we can use it in our tests.
@@ -19,13 +20,30 @@ describe('Posts', function() {
       url: 'https://www.google.com',
       summary: 'post summary'
   };
+  const user = {
+      username: 'poststest',
+      password: 'testposts'
+  };
+  before(function (done) {
+    agent
+      .post('/sign-up')
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send(user)
+      .then(function (res) {
+          done();
+      })
+      .catch(function (err) {
+          done(err);
+      });
+  });
+
   it("should create with valid attributes at POST /post/new", function (done) {
     // Checks how many posts there are now
     Post.estimatedDocumentCount()
         .then(function (initialDocCount) {
             chai
                 .request(app)
-                .post("/posts/new")
+                .post("/post/new")
                 // This line fakes a form post,
                 // since we're not actually filling out a form
                 .set("content-type", "application/x-www-form-urlencoded")
@@ -54,5 +72,21 @@ describe('Posts', function() {
     });
 });
 after(function () {
-    Post.findOneAndDelete(newPost);
+    Post.findOneAndDelete(newPost)
+    .then(function (res) {
+        agent.close()
+
+        User.findOneAndDelete({
+            username: user.username
+        })
+          .then(function (res) {
+              done()
+          })
+          .catch(function (err) {
+              done(err);
+          });
+    })
+    .catch(function (err) {
+        done(err);
+    });
 });
